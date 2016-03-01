38d3f71c289579126cee66b4ad7063a3d7fbb8d9
Roland Vachter <roland.vachter@ft.com>
2016-03-01 19:05:01 +0200

    Added LCD test script.

---
+++ lcd.js
@@ -0,0 +1,25 @@
+var Lcd = require('lcd'),
+	lcd = new Lcd({
+		rs: 12,
+		e: 16,
+		data: [5, 6, 13, 19],
+		cols: 16,
+		rows: 1
+	});
+
+lcd.on('ready', function() {
+	setInterval(function() {
+		lcd.setCursor(0, 0);
+		lcd.print(new Date().toISOString().substring(11, 19), function(err) {
+			if (err) {
+				throw err;
+			}
+		});
+	}, 1000);
+});
+
+// If ctrl+c is hit, free resources and exit. 
+process.on('SIGINT', function() {
+	lcd.close();
+	process.exit();
+});
